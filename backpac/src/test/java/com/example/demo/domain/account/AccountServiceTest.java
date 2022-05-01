67dc235ab4479b2ff7c51b3d3438de4cf09f7396
kimso <kimso0907@gmail.com>
2022-05-01 10:11:25 +0900

    Add - 로그인 API service 테스트케이스 추가

--- backpac/src/test/java/com/example/demo/domain/account/AccountServiceTest.java
+++ backpac/src/test/java/com/example/demo/domain/account/AccountServiceTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.example.demo.domain.account.request.LogInRequestDto;
 import com.example.demo.domain.account.request.SignUpRequestDto;
 import com.example.demo.global.dto.response.BackpacResponseData;
 import com.example.demo.global.dto.response.ErrorData;
@@ -21,8 +23,11 @@ public class AccountServiceTest {
     @Autowired
     private AccountService service;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
     @MockBean
-    private AccountRepository accountRepository;
+    private AccountRepository repository;
     
     @Test
     @DisplayName("회원가입 성공")
@@ -37,7 +42,7 @@ public class AccountServiceTest {
     	param.setSex("M");
     	param.setType("user");
 
-	    when(accountRepository.findByEmail(any())).thenReturn(null);
+	    when(repository.findTop1ByEmailOrNickname(any(), any())).thenReturn(null);
 	    
 	    // then
 	    BackpacResponseData signupResult = service.signup(param);
@@ -59,10 +64,73 @@ public class AccountServiceTest {
 
         // 회원가입 대상
         AccountEntity accountMock = new AccountEntity();
-        when(accountRepository.findByEmail(any())).thenReturn(accountMock);
+        accountMock.setEmail("testRegNo@email.com");
+        accountMock.setNickname("testnckname@email.com");
+        when(repository.findTop1ByEmailOrNickname(any(), any())).thenReturn(accountMock);
         
         // then
         BackpacResponseData signupResult = service.signup(param);
         assertThat(signupResult).isExactlyInstanceOf(ErrorData.class);
+    }
+    
+    @Test
+    @DisplayName("로그인성공") 
+    public void loginSuccess() throws Exception {
+
+        LogInRequestDto param = new LogInRequestDto();
+        param.setEmail("test@email.com");
+        param.setPassword("passTest");
+        
+        // 유저 정보 조회
+        AccountEntity accountMock = new AccountEntity();
+        accountMock.setEmail("test@email.com");
+        accountMock.setPassword(passwordEncoder.encode("passTest"));
+
+        when(repository.findByEmail(any())).thenReturn(accountMock);
+        
+        // then
+        BackpacResponseData loginResult = (BackpacResponseData)service.login(param);
+        
+        assertThat(loginResult).isInstanceOf(SuccessData.class);
+    }
+    
+    @Test
+    @DisplayName("로그인실패(잘못된 아이디)") 
+    public void loginFail() throws Exception {
+
+        LogInRequestDto param = new LogInRequestDto();
+        param.setEmail("test@email.com");
+        param.setPassword("passTest");
+        
+        // 유저 정보 조회
+        AccountEntity accountMock = new AccountEntity();
+        accountMock.setEmail("test@email.com");
+        accountMock.setPassword(passwordEncoder.encode("passTest"));
+
+        when(repository.findByEmail(any())).thenReturn(null);
+        
+        // then
+        ErrorData loginResult = (ErrorData)service.login(param);
+        assertThat(loginResult.getErrorMessage()).isEqualTo("존재하지 않는 아이디입니다.");
+    }
+    
+    @Test
+    @DisplayName("로그인실패(잘못된 비밀번호)") 
+    public void loginFail2() throws Exception {
+
+        LogInRequestDto param = new LogInRequestDto();
+        param.setEmail("test@email.com");
+        param.setPassword("passTest");
+        
+        // 유저 정보 조회
+        AccountEntity accountMock = new AccountEntity();
+        accountMock.setEmail("test@email.com");
+        accountMock.setPassword(passwordEncoder.encode("passTestError"));
+
+        when(repository.findByEmail(any())).thenReturn(accountMock);
+        
+        // then
+        ErrorData loginResult = (ErrorData)service.login(param);
+        assertThat(loginResult.getErrorMessage()).isEqualTo("비밀번호를 확인해 주세요.");
     }
 }
